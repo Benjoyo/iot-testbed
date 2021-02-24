592f49bbb425040b137c0cdcd9951bc293a47a6d
Bennet <bennet.krause@gmail.com>
2021-02-24 16:57:23 +0100

    Update trace.sh

--- raspi/scripts/trace.sh
+++ raspi/scripts/trace.sh
@@ -6,4 +6,4 @@ export PATH=$PATH:/home/user/scripts/nrf52/JLink_Linux_arm
 
 cd tracing
 
-java -jar client.jar -n $HOSTNAME -s $1 -d NRF52840_XXAA &
+java -jar client.jar -n $HOSTNAME -s $1 -d NRF52840_XXAA -m &
